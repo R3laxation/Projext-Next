49424784ae5e063b370f7b43b829bd77319cc562
R3laxation <memories@bk.ru>
2022-09-05 13:10:48 +0600

    Added universal button component Button

---
+++ components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import styles from './Button.module.css';
+import {ButtonProps} from "./ButtonProps";
+import cn from 'classnames';
+
+export const Button = ({appearance, children, className, ...props}: ButtonProps): JSX.Element => {
+
+    return (
+        <button className={cn(styles.button, {
+            [styles.primary]: appearance === 'primary',
+            [styles.ghost]: appearance === 'ghost'
+        })} {...props}>{children}</button>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from "../components";
 import {Htag} from "../components/Htag/Htag";
 
 export default function Home() {
   return (
     <div>
         <Htag tag='h1'>ts</Htag>
+        <Button appearance={'primary'}>Кнопка</Button>
     </div>
   )
 }
