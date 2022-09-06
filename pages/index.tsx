58330c67516570c73d72663dce2621c1185925b9
R3laxation <memories@bk.ru>
2022-09-06 19:21:27 +0600

    Added counterState

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import {Button, Paragraph, Tag} from "../components";
 import {Htag} from "../components/Htag/Htag";
 
 export default function Home() {
+
+    const [counter, setCounter] = useState(0);
+
   return (
     <div>
-        <Htag tag='h1'>ts</Htag>
-        <Button appearance={'primary'}  arrow={'right'}>Кнопка</Button>
+        <Htag tag='h1'>{counter}</Htag>
+        <Button appearance={'primary'} arrow={'right'} onClick={() => setCounter((counter) => counter +1)}>Кнопка</Button>
         <Paragraph>asd</Paragraph>
         <Tag size={'s'}>Small</Tag>
         <Tag size={'m'} color={'red'}>Medium</Tag>
