f84729cb8cf2e5b57b60a909e3583914f32480c5
R3laxation <memories@bk.ru>
2022-09-10 18:13:10 +0600

    Added footer styles

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -11,6 +11,8 @@ export const Footer = ({className, ...props}: FooterProps): JSX.Element => {
                 <div>
                     OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
                 </div>
+                <a href="#" target='_blank'>Пользовательское соглашение</a>
+                <a href="#" target='_blank'>Политика конфиденциальности</a>
             </footer>
     )
 }
