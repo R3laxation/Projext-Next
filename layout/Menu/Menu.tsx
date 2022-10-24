3e3f4b326e286ff1bcb9ab18f3f1a3d6bffc0603
R3laxation <memories@bk.ru>
2022-10-24 13:57:22 +0600

    Updated Menu.tsx

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,5 +1,18 @@
 import {useContext, useEffect} from "react";
 import {AppContext} from "../../context/app.context";
+import {FirstLevelMenuItem} from "../../intergaces/menu.interface";
+import CoursesIcon from './Icons/courses.svg';
+import ServicesIcon from './Icons/services.svg.svg';
+import BooksIcon from './Icons/books.svg.svg';
+import GoodsIcon from './Icons/goods.svg.svg';
+import {TopLevelCategory} from "../../intergaces/page.interface";
+
+const firstLevelMenu: FirstLevelMenuItem[] = [
+    {route: 'courses', name: 'Курсы', icon: <CoursesIcon/>, id: TopLevelCategory.Courses},
+    {route: 'services', name: 'Курсы', icon: <ServicesIcon/>, id: TopLevelCategory.Courses},
+    {route: 'books', name: 'Курсы', icon: <BooksIcon/>, id: TopLevelCategory.Courses},
+    {route: 'goods', name: 'Курсы', icon: <GoodsIcon/>, id: TopLevelCategory.Courses},
+];
 
 export const Menu = (): JSX.Element => {
     const {menu, setMenu, firstCategory} = useContext(AppContext);
