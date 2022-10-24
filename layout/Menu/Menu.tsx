08bd344c1c3af3f4b78d6326585e3ff3f612fb76
R3laxation <memories@bk.ru>
2022-10-24 13:05:11 +0600

    Updated Menu.tsx

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,12 +1,11 @@
-import cn from 'classnames';
-import styles from './Menu.module.css';
-import {format} from 'date-fns';
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import {AppContext} from "../../context/app.context";
-
 
 export const Menu = (): JSX.Element => {
     const {menu, setMenu, firstCategory} = useContext(AppContext);
+    useEffect(() => {
+        setMenu && setMenu([]);
+    });
 
     return (
         <div>
