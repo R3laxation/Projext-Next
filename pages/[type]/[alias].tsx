a5b5d00a17fe4214a8a10f08a2bd497380ee2cfc
R3laxation <memories@bk.ru>
2022-10-25 19:46:20 +0600

    Fixed alias.tsx, added try, catch

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -25,7 +25,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
         const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
             firstCategory: m.id
         });
-        paths: paths.concat(menu.flatMap(m => m.pages.map(p => '/[type]/' + p.alias)));
+        paths: paths.concat(menu.flatMap(s => s.pages.map(p => `/${m.route}/${p.alias}`)));
     }
 
     return {
@@ -50,22 +50,32 @@ export const getStaticProps: GetStaticProps<CourseProps> = async ({params}: GetS
         }
     }
 
-    const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
-        firstCategory: firstCategoryItem.id
-    });
-    const {data: page} = await axios.get<TopPageModel>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/byAlias/' + params.alias);
-    const {data: products} = await axios.post<ProductModel[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/product/find', {
-        category: page.category,
-        limit: 10,
-    });
+    try {
+        const {data: menu} = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+            firstCategory: firstCategoryItem.id
+        });
+        if(menu.length === 0){
+            return {
+                notFound: true
+            }
+        }
+        const {data: page} = await axios.get<TopPageModel>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/byAlias/' + params.alias);
+        const {data: products} = await axios.post<ProductModel[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/product/find', {
+            category: page.category,
+            limit: 10,
+        });
 
-
-    return {
-        props: {
-            menu,
-            firstCategory: firstCategoryItem.id,
-            page,
-            products,
+        return {
+            props: {
+                menu,
+                firstCategory: firstCategoryItem.id,
+                page,
+                products,
+            }
+        }
+    } catch (e) {
+        return {
+            notFound: true
         }
     }
 }
