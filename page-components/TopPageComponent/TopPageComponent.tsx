46d70ba2ffd314be2ec31659ca212e988c318ba9
R3laxation <memories@bk.ru>
2022-10-27 12:36:13 +0600

    Added title in TopPageComponent.tsx

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,12 +1,18 @@
 import cn from 'classnames';
 import {TopPageComponentProps} from "./TopPageComponentProps";
 import styles from './TopPageComponent.module.css';
+import {Htag, Tag} from "../../components";
 
 
 export const TopPageComponent = ({page, products, firstCategory}: TopPageComponentProps): JSX.Element => {
 
     return (
             <>
+                <div >
+                    <Htag tag={'h1'}>{page.title}</Htag>
+                    {products && <Tag color={'grey'} size={'m'}>{products.length}</Tag>}
+                    <span></span>
+                </div>
                 {products && products.length}
             </>
     )
