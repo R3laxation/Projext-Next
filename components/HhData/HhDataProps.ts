adbb8f622f2bd7c817b0d95c06a2503242fe1bc9
R3laxation <memories@bk.ru>
2022-10-31 19:53:50 +0600

    Updated HhDataProps.ts

--- components/HhData/HhDataProps.ts
+++ components/HhData/HhDataProps.ts
@@ -1,7 +1,6 @@
 import {DetailedHTMLProps, HTMLAttributes, ReactNode} from "react";
+import {HhData} from "../../interfaces/page.interface";
 
-export interface HhDataProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>{
-    color?: 'white' | 'blue';
-    children: ReactNode;
+export interface HhDataProps extends HhData{
 
 }
