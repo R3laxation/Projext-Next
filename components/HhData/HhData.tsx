400bcee4e132b434ef2bb2be5df11f21753c56e0
R3laxation <memories@bk.ru>
2022-10-31 20:12:56 +0600

    Added icons in HhData.tsx

--- components/HhData/HhData.tsx
+++ components/HhData/HhData.tsx
@@ -1,6 +1,7 @@
 import styles from './HhData.module.css';
 import {Card} from "../Card/Card";
 import {HhDataProps} from "./HhDataProps";
+import RateIcon from './rate.svg';
 
 export const HhData = ({count}: HhDataProps): JSX.Element => {
 
@@ -13,6 +14,11 @@ export const HhData = ({count}: HhDataProps): JSX.Element => {
                 <Card className={styles.salary}>
                     <div className={styles.title}>Всего вакансий</div>
                     <div className={styles.salaryValue}>{count}</div>
+                    <div className={styles.rate}>
+                        <RateIcon/>
+                        <RateIcon/>
+                        <RateIcon/>
+                    </div>
                 </Card>
             </div>
     )
